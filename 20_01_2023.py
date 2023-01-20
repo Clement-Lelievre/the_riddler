e0587998af3e3d848c1c1323b8308e5b9b43da48
clementlelievre <clement.lelievre90@gmail.com>
2023-01-20 15:46:52 +0100

    riddler express 01/20/2023

--- 20_01_2023.py
+++ 20_01_2023.py
@@ -1,23 +1,21 @@
 from itertools import combinations, starmap
+from math import prod
 
 
 def is_valid(date1: tuple[int, int], date2: tuple[int, int]) -> bool:
-    """Returns whether  at least one pf the two dates contain both the sum and product of the other date
+    """Returns whether at least one of the two dates contains both the sum and
+    the product of the other date
 
     Args:
-        date1 (tuple[int, int]): _description_
-        date2 (tuple[int, int]): _description_
+        date1 (tuple[int, int])
+        date2 (tuple[int, int])
 
     Returns:
-        bool: _description_
+        bool
     """
-    m1, d1 = date1
-    m2, d2 = date2
-    s1, p1 = m1 + d1, m1 * d1
-    s2, p2 = m2 + d2, m2 * d2
-    return (s1 in date2 and p1 in date2) | (s2 in date1 and p2 in date1)
+    return (sum(date1) in date2 and prod(date1) in date2) | (sum(date2) in date1 and prod(date2) in date1)
 
 
-max_day_in_month = {k: 31 if k % 2 else 30 for k in range(1, 13)} # adding Feb 30th doesn't change the end result
+max_day_in_month = {k: 31 if k % 2 else 30 for k in range(1, 13)}  # adding Feb 30th doesn't change the end result
 all_dates = ((mm, dd) for mm in range(1, 13) for dd in range(1, max_day_in_month[mm] + 1))
 print(sum(starmap(is_valid, combinations(all_dates, 2))))
