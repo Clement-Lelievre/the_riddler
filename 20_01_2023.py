c6210541a012c96119244ae1b7e4473be8c53a71
clementlelievre <clement.lelievre90@gmail.com>
2023-01-27 17:17:47 +0100

    correct my code for the count of pairs of dates

--- 20_01_2023.py
+++ 20_01_2023.py
@@ -13,9 +13,17 @@ def is_valid(date1: tuple[int, int], date2: tuple[int, int]) -> bool:
     Returns:
         bool
     """
-    return (sum(date1) in date2 and prod(date1) in date2) | (sum(date2) in date1 and prod(date2) in date1)
+    s1, s2, p1, p2 = sum(date1), sum(date2), prod(date1), prod(date2)
+    return set(date1) == set((s2, p2)) or set(date2) == set((s1, p1))
+    # return (sum(date1) in date2 and prod(date1) in date2) or (sum(date2) in date1 and prod(date2) in date1) # this (my initial code)
+    # is wrong because for the case 2/2 as the sum = the product, I gave too much leeway to the other date
+    # ie the 4 was contained in excactly the m or the d but not both
 
 
-max_day_in_month = {k: 31 if k % 2 else 30 for k in range(1, 13)}  # adding Feb 30th doesn't change the end result
-all_dates = ((mm, dd) for mm in range(1, 13) for dd in range(1, max_day_in_month[mm] + 1))
+max_day_in_month = {
+    k: 31 if k % 2 else 30 for k in range(1, 13)
+}  # adding Feb 30th doesn't change the end result
+all_dates = (
+    (mm, dd) for mm in range(1, 13) for dd in range(1, max_day_in_month[mm] + 1)
+)
 print(sum(starmap(is_valid, combinations(all_dates, 2))))
